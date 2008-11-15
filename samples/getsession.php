a1a9e87e9625392a3863e0d1ee6307955b3ff1cb
stephen.marquard@uct.ac.za <stephen.marquard@uct.ac.za@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-11-15 14:59:41 +0000

    SAK-14900 Add verification code for getsession method
    git-svn-id: https://source.sakaiproject.org/svn/linktool/trunk@54323 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samples/getsession.php
+++ samples/getsession.php
@@ -1,40 +1,31 @@
 <?
 
-// Script for testing Sakai linktool. This is https://source.sakaiproject.org/svn/linktool/trunk/samples/linktool.php
+header('Content-type: text/html; charset=UTF-8') ;
+
+// Script for testing Sakai linktool getsession requests.
+// This is https://source.sakaiproject.org/svn/linktool/trunk/samples/getsession.php
 
 // Requires PEAR SOAP module, and php modules curl, openssl, zlib (for installing PEAR modules if required)
 
-// This script can be invoked from Sakai in a popup window (for example in a Melete unit)
-// with a URL such as:
-//    https://sakai.domain/sakai-rutgers-linktool?site=SITEID&url=SCRIPTURL&param1=value1&param2=value2
-// where SITEID = Sakai Site ID of the site in which the URL is placed, SCRIPTURL = url of this script,
-// param1 and param2 are optional application-specific parameters.
+// This script is called from the verification script linktool.php
 
 // Adding PEAR to the include_path may be required depending on local php config
 // ini_set("include_path","/usr/share/php");
 
-header('Content-type: text/html; charset=UTF-8') ;
-
 // Show errors
 error_reporting(E_ALL^E_NOTICE);
 
-// Script path
-  $signingurl = "SakaiSigning.jws?wsdl";
-
 // Get the linktool parameters
-  $user = strip_tags($_GET['internaluser']);
-  $euid = strip_tags($_GET['user']);
-  $site = strip_tags($_GET['site']);
-  $server = strip_tags($_GET['serverurl']);
-  $sessionid = strip_tags($_GET['session']);
-  $placement = strip_tags($_GET['placement']);
-  $role = strip_tags($_GET['role']);
-  $sign = strip_tags($_GET['sign']);
-  $time = strip_tags($_GET['time']);
+  $signedobject = urldecode(strip_tags($_GET['signedobject']));
+  $querystring = urldecode(strip_tags($_GET['querystring']));
+  $server= urldecode(strip_tags($_GET['server']));
 
   $url = $server . "/sakai-axis/";
 
+  $signingurl = "SakaiSigning.jws?wsdl";
+
   $result = "";
+  $displayname = "";
 
 // Check for required variables
 if ($server != "") {
@@ -46,7 +37,20 @@ if ($server != "") {
 	$result = "SOAP Error";
   } else {
   	// verify the arguments passed to us
-  	$result=$signingProxy->testsign($_SERVER['QUERY_STRING']); 
+  	$result=$signingProxy->getSessionToServer($querystring, $signedobject); 
+  }
+
+  list($sessionid, $newserver) = split(",", $result);
+
+  $url2 = $newserver . "/sakai-axis/";
+
+  // Get the current user's display name to verify that the session is ok
+  $sakaiscript = getproxy($url2, "SakaiScript");
+
+  if (PEAR::isError($sakaiscript)) {
+	$result = "SOAP Error";
+  } else {
+	$displayname = $sakaiscript->getCurrentUserDisplayName($sessionid);
   }
 
 }
@@ -154,77 +158,32 @@ This script is to test the functionality of the Sakai LinkTool from QA and other
 <table class="listHier" cellspacing="0" border="0" summary="Parameters">
 <tbody>
 <tr><th>Parameter</th><th>Value</th><th>Description</th></tr>
-<tr><td>user</td><td><?=$euid?></td><td>Sakai enterprise id (eid)</td></tr>
-<tr><td>internaluser</td><td><?=$user?></td><td>Sakai internal id (userid)</td></tr>
-<tr><td>site</td><td><?=$site?></td><td>Sakai site id</td></tr>
-<tr><td>role</td><td><?=$role?></td><td>Role in the site</td></tr>
-<tr><td>sessionid</td><td><?=$sessionid?></td><td>Encrypted session id</td></tr>
-<tr><td>serverurl</td><td><?=$server?></td><td>URL of the calling server</td></tr>
-<tr><td>time</td><td><?=$time?></td><td>Time that script was invoked</td></tr>
-<tr><td>placement</td><td><?=$placement?></td><td>Sakai tool placement id (2.5 and later)</td></tr>
-<tr><td>sign</td><td><?=$sign?></td><td>Linktool parameter signature</td></tr>
+<tr><td>server</td><td><?=$server?></td><td>Server</td></tr>
+<tr><td>signedobject</td><td><?=$signedobject?></td><td>Linktool signed object</td></tr>
+<tr><td>querystring<site</td><td><?=$querystring?></td><td>Query string passed to app by Linktool</td></tr>
 </tbody>
 </table>
-
-<h3>Application Parameters</h3>
-
-<? 
- unset($_GET['internaluser']);
- unset($_GET['user']);
- unset($_GET['site']);
- unset($_GET['role']);
- unset($_GET['session']);
- unset($_GET['serverurl']);
- unset($_GET['time']);
- unset($_GET['placement']);
- unset($_GET['sign']);
-
- if (count($_GET) > 0) {
-
-?>
-<table class="listHier" cellspacing="0" border="0" summary="Parameters">
-<tbody>
-<tr><th>Parameter</th><th>Value</th></tr>
-<?
-
- foreach (array_keys($_GET) as $key) {
-   print "<tr><td>" . htmlspecialchars($key) . "</td><td>" . htmlspecialchars($_GET[$key]) . "</td></tr>";
- }
-
- print "</tbody></table>";
-
-} else { 
- print "None.";
-}
-
-?>
 
 <h3>Server callback</h3>
 
 <table class="listHier" cellspacing="0" border="0" summary="Results">
 <tbody>
 <tr><td>Callback service</td><td><a href="<?=$url . $signingurl?>"><?=$url . $signingurl?></a></td></tr>
-<tr><td>Response</td><td><?=$result?></td></tr>
+<tr><td>Session id</td><td><?=$sessionid?></td></tr>
+<tr><td>Server</td><td><?=$newserver?></td></tr>
+<tr><td>Display name</td><td><?=htmlspecialchars($displayname)?></td></tr>
 </tbody>
 </table>
 
 <? 
 
-$msg = "Unknown error";
+$msg = "";
 
 if ($server == "") {
 
-  $msg = "No server was specified - perhaps this script was not called by LinkTool.";
+  $msg = "No server was specified - perhaps this script was not called by the LinkTool verification script.";
 
 } else {
-
-	if ($result == "true") {
-		$msg = "All correct - the verification request was authorized by the callback web service.";
-	}
-
-	if ($result == "false") {
-		$msg = "The callback service did not authenticate the request.";
-	}
 
 	if ($result == "not enabled") {
 		$msg = "The callback service is not enabled. Sakai 2.5 and later requires the sakai.properties setting <strong>linktool.enabled=true</strong>";
@@ -234,22 +193,22 @@ if ($server == "") {
 		$msg = "Could not connect to the callback web service. Check to see if the Callback service URL is correct.";
 	}
 
-	if ($result == "stale value") {
-		$msg = "The verification token has expired - possibly the same request URL has been reused later from another source.";
+	if (!defined($sessionid)) {
+		$msg = "Did not get a valid session id from the web service. The signed object may be invalid.";
+	}
+
+	if (strlen($signedobject) == 0) {
+		$msg = "No signed object was specified. Perhaps this script was not called by the LinkTool verification script.";
+	}
+
+	if (strlen($displayname) > 0) {
+		$msg = "OK";
 	}
 }
 
 ?>
 <h3>Results</h3>
 <p><?=$msg?></p>
-<h3>Get session using signed object</h3>
-<form action="getsession.php" method="get">
-<label for="signedobject">Enter a signed object from LinkTool (Setup / Generate Signed Object)</label>
-<p><input id="signedobject" type="text" name="signedobject" size="80"/></p>
-<input id="querystring" type="hidden" name="querystring" value="<?= urlencode($_SERVER['QUERY_STRING']) ?>"/>
-<input id="server" name="server" type="hidden" value="<?= urlencode($server) ?>"/>
-<p class="act"><input type="submit" value="Request session"/></p>
-</form>
 </div>
 </body>
 </html>
